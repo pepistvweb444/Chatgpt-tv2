47c420fc22b148867dc2b0cc6c75aa0d00e6f21d
pepistvweb444 <alberto@albertoramos.name>
2026-08-21 17:33:47 +0200

    build: configure Jarvis TV v0.2 app

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.jarvis.tv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jarvis.tv"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.leanback:leanback:1.2.0")
+}
