093b0bdc242283ba8e71e3526e8f96927d0e8f2e
pepistvweb444 <alberto@albertoramos.name>
2026-08-22 19:32:38 +0200

    mobile: bump Jarvis Mobile to 0.6.4

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jarvis.mobile.stable"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.6.4"
     }
 
     compileOptions {
