f3079e52e329e22e1fc021e2cbca481fb25ac52d
pepistvweb444 <alberto@albertoramos.name>
2026-08-29 19:45:26 +0200

    mobile: bump Jarvis to 0.2.18

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jarvis.mobile.stable"
         minSdk = 28
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.2.17"
+        versionCode = 18
+        versionName = "0.2.18"
     }
 
     compileOptions {
