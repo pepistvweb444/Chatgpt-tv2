d8471bcfcc70a770a0e0cac2e683a07ba828c526
pepistvweb444 <alberto@albertoramos.name>
2026-09-03 19:45:21 +0200

    Bump Jarvis TV to 0.6.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jarvis.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.6.11"
+        versionCode = 19
+        versionName = "0.6.12"
     }
 
     compileOptions {
